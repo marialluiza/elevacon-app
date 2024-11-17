11cfc5ec56a9b98c5a880ffcee8e8077c6e9fa20
Gabriel <gabrielnino@edu.unifil.br>
2024-11-17 18:02:50 -0300

    Configuracao de MUI e Dashboard

---
+++ client/src/interfaces/IMediaCard.tsx
@@ -0,0 +1,8 @@
+export interface IMediaCardProps {
+  title: string;
+  shareText?: string;
+  learnMoreText?: string;
+  badgeContent?: number | string;
+  shareRoute?: string;
+  learnMoreRoute?: string;
+}
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import '@radix-ui/themes/styles.css';
-import { Theme } from '@radix-ui/themes';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
 
 ReactDOM.render(
   <React.StrictMode>
-    
     <Theme>
       <App />
     </Theme>
-    
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./App";
+// // import './index.css';
+// // import '@radix-ui/themes/styles.css';
+// // import { Theme } from '@radix-ui/themes';
+// import theme from "./theme/theme";
+// import { ThemeProvider } from "@mui/material/styles";
+// import { StyledEngineProvider } from "@mui/material/styles";
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     {/* <StyledEngineProvider injectFirst> */}
+//     <ThemeProvider theme={theme}>
+//       <App />
+//     </ThemeProvider>
+//     {/* </StyledEngineProvider> */}
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
