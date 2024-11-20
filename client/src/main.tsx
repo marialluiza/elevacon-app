4e139472fb9d1f05653b0789604fa36499fbd03e
marialluiza <malululicha@gmail.com>
2024-11-20 16:14:19 -0300

    bibs

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,17 +3,16 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 import "@radix-ui/themes/styles.css";
-import { Theme, ThemePanel } from "@radix-ui/themes";
+import { Theme } from "@radix-ui/themes";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme/theme";
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* Envolvendo o App com ambos os provedores de tema */}
     <Theme>
       <ThemeProvider theme={theme}>
         <App />
-        <ThemePanel></ThemePanel>
+        {/* <ThemePanel></ThemePanel> */}
       </ThemeProvider>
     </Theme>
   </React.StrictMode>,
